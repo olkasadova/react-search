dfcc8bf0111ddfcf50406193989deced4d721fd6
Olha Sadova <108397789+olkasadova@users.noreply.github.com>
2024-03-03 23:55:26 +0200

    Fixed footer link to Github

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,3 @@
-import react from "react";
 
 export default function Footer (){
     return(
@@ -6,8 +5,8 @@ export default function Footer (){
         <p>
           This project was coded by
           <a href="#" target="_blank"> Olha Sadova</a> and is
-          <a href="https://github.com/olkasadova" target="_blank" rel="noopener"> on GitHub</a> and
-          <a href="https://clever-kleicha-669fcd.netlify.app/" target="_blank" rel="noopener"> hosted on Netlify</a>
+          <a href="https://github.com/olkasadova/react-weather" target="_blank" rel="noopener"> on GitHub</a> and
+          <a href="https://react-vanilla-search.netlify.app/" target="_blank" rel="noopener"> hosted on Netlify</a>
         </p>
       </footer>
       );
