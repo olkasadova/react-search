d434eb58efeba33bff36f448fef41e0b595c3ae6
Olha Sadova <108397789+olkasadova@users.noreply.github.com>
2024-03-04 00:32:09 +0200

    Fixed link in href

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer (){
 <footer>
         <p>
           This project was coded by
-          <a href="#" target="_blank"> Olha Sadova</a> and is
+          <a href="https://react-vanilla-search.netlify.app/" target="_blank" rel="noreferrer"> Olha Sadova</a> and is
           <a href="https://github.com/olkasadova/react-weather" target="_blank" rel="noopener"> on GitHub</a> and
           <a href="https://react-vanilla-search.netlify.app/" target="_blank" rel="noopener"> hosted on Netlify</a>
         </p>
